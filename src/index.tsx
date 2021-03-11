0e7a6b9725f62011feaa44398bab705e13095fd4
ustsepavy <vladstepovoyvios@gmail.com>
2021-03-11 17:09:28 +0300

    feat: i18n initialized

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 import store from './redux/Store';
+import './index.css';
+import 'antd/dist/antd.css';
+import App from './App';
 
 const renderer = () => {
   ReactDOM.render(
     <React.StrictMode>
       <BrowserRouter>
-        <Provider store={store}>
-          <App />
-        </Provider>
+        <I18nextProvider i18n={i18n}>
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </I18nextProvider>
       </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')

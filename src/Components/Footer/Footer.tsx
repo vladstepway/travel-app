3b72dd3cdf0b9d3dfbd5d9b414f6cfc49c341b65
Ilya Barachenia <ilya.barachenia@gmail.com>
2021-03-18 01:57:27 +0100

    fix: fixed footer styles

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -5,10 +5,11 @@ import styles from './Footer.module.css'
 const Footer = function() {
   return <div className={styles.footer}><span className='footer__year'>2021</span>
     <a className='footer__author-link' href='https://github.com/vladstepway'target='_blank'>Vladislav Stepovoy</a>
-    <a className='footer__author-link' href='https://github.com/SixStringer91' target='_blank'>Ilya Barachenia</a>
-    <a className='footer__author-link' href='https://github.com/juliememe' target='_blank'>Danik Lutsev</a>
+    <a className='footer__author-link' href='https://github.com/mykamapolice' target='_blank'>Ilya Barachenia</a>
+    <a className='footer__author-link' href='https://github.com/SixStringer91' target='_blank'>Danik Lutsev</a>
     <a className='footer__author-link' href='https://github.com/svdfsdev' target='_blank'>Vasiliy Suhovenko</a>
     <img
+      style={{height: "2rem"}}
       className={styles.imgFooter}
       src="https://rs.school/images/rs_school_js.svg"
       alt="rs-logo"
